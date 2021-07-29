69645e0f8e42effa7925c7b44a16fc1f864f31f8
DegnKristine <84312237+DegnKristine@users.noreply.github.com>
2021-07-29 15:01:39 +0200

    update of file - Mostly problem description.

--- evaluate_mutations.py
+++ evaluate_mutations.py
@@ -1,16 +1,27 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
+"""
+This file contains the code required to answer
+the question: 
+
+Does the found structure contain the mutations?
+
+"""
+
+##############
+# Import packs
+##############
 import pandas as pd
 import os
 from Bio.PDB import *
 from Bio import SeqIO
 
+##############
+
 
 def import_csv(Path, pdb_uniprot_df):
-    """Returns..."""
+    """This function takes the path of and csv file created in the 
+    validate_sequence.py script, the pdb_uniprot_df and returns it as 
+    a dataframe."""
 
     #ensure correct placement
     os.chdir(Path)
@@ -22,7 +33,9 @@ def import_csv(Path, pdb_uniprot_df):
     return pdb_df
 
 def merge_information(start_df, pdb_df):
-    """Returns..."""
+    """This function takes pdb_uniprot_df (pdb_df) imported in import_csv()
+    and merge the information with the input file to return a singular 
+    dataframe containing all the relevant information"""
     
     #start by removing the uniprots with no related PDBs 
     new_df = start_df[start_df["Uniprot"].isin(list(pdb_df["Uniprot"]))]
@@ -33,6 +46,27 @@ def merge_information(start_df, pdb_df):
     return df
 
 def get_sequence(df, path):
+    
+    """This function takes the dataframe created in merge_information() 
+    as input.
+    
+    The aim is to import the sequence of amino acids from the PDB file and
+    use the mutational position to check if the mutation is present in the 
+    PDB file. However, the sequence I import below is not the same in length
+    as the segment ranges I have used to find the domains. Therefore 
+    there a discrepency that I don't have an answer as to why it is there. 
+    
+    Planed actions: 
+        
+        - Revisit the way I retrive the segments from slimfast, perhaps
+            I have not really understood and therefore I have a problem. 
+        - PDB versus PDBe 
+    
+    The output is supposed to be an additional column to the merged df
+    which includes information about the specific mutation, if present 
+    or not. 
+    
+    """
     
     pdb = PDBList()
     
@@ -60,8 +94,3 @@ def get_sequence(df, path):
     seq_df = pd.DataFrame(d)
     
     return seq_df
-
-
-######## there is something here that does not add up biologically. 
-# unfortunately I think the problem is in the way i have constructed the ranges. 
-
