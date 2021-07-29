4a601cd530e5e202091d87c683ab0f20ba0fe541
DegnKristine <84312237+DegnKristine@users.noreply.github.com>
2021-07-29 15:03:08 +0200

    update of choose_model.py (0,02)

--- choose_model.py
+++ choose_model.py
@@ -2,5 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Work not started
+
+However, this script ought to become obsolete due to the sorting of the files in find_structure_list. 
+
+Will be deleted when this section has been finalized. 
+
 """
 
