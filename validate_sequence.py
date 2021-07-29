c0ab5252e5974faece0036b4d343322636b7e3df
DegnKristine <84312237+DegnKristine@users.noreply.github.com>
2021-07-29 14:51:01 +0200

    update validate_sequence (v0,02)

--- validate_sequence.py
+++ validate_sequence.py
@@ -1,6 +1,15 @@
+
 """
-DESRIPTION TO BE UPDATED 
+This file contains the code required to answer
+the question: 
+
+Does the found structure contain the sequence where the mutations are?
+
 """
+
+##############
+# Import packs
+##############
 
 import pandas as pd
 import numpy as np
@@ -11,7 +20,14 @@ import os
 import re
 import glob
 
+##############
+
 def import_names(Path):
+    """Function that takes the path and returns two lists, 
+    names list =  A list of all he PDBs related to a single Uniprot ID
+    Uniprot_id = A list with the uniprot IDs to which there are solved structures. 
+    """
+    
     #create an empty name list for each uniprot ID with corresponding pdbs
     names_list = []
     
@@ -32,7 +48,9 @@ def import_names(Path):
     
 
 def import_csvs(Path, names_list, uniprot_id):
-    """Returns a list, where each item is a pandas dataframe containing the list of pdb sturctures"""
+    """Takes the path, and the output from import_names and 
+    Returns a list, where each item is a pandas dataframe 
+    containing the list of pdb sturctures"""
     
     #create an empty list for all the dataframes to be imported into.
     dataframes_list = []
@@ -53,7 +71,9 @@ def import_csvs(Path, names_list, uniprot_id):
 #Create a dictionary with the uniprot ID and mutational sites.
 
 def seq_pos_dict(input_dataframe):
-    """returns a dictionary with the uniprot ID as key and all the positions of the mutations as values"""
+    """Takes the input file as inpit and returns a dictionary with 
+    the uniprot ID as key and all the positions of the mutations as 
+    values"""
     
     #make a list of all uniprotIDs
     IDs = list(input_dataframe.Uniprot)
@@ -77,6 +97,11 @@ def seq_pos_dict(input_dataframe):
  ################################################################################
  
 def find_ranges(segments):
+    """The function takes the segments found in get_uniprot_segments() 
+    (SLIM) which is ranges where the uniprot ID is covered by a given PDB 
+    file. find_ranges() removes the segments that are not covered by the 
+    PDB and output an array of the AA range the specific PDB covers"""
+    
     #output from get_uniprot_segments
     
     segment_array = np.array(list(segments.values()))
@@ -87,14 +112,19 @@ def find_ranges(segments):
     #create empty list for removal:
     to_be_removed = []
     
+    #control if any ranges are not covered by the PDB
     for i in range(len(segment_array)):
         if None in segment_array[i]:
             to_be_removed.append(i)
         else:
             segment_array[i] == segment_array[i]
     
+    #create a list of the segments the PDB does not cover if any. 
+    #e.g. a chain not modelled. 
     tbr = np.array(to_be_removed)
     
+    #if the list of uniprot segments that are not covered by the PDB
+    #is longer than 0 some segments are to be removed. 
     if len(tbr) > 0:
         segment_array = np.delete(segment_array, tbr, 0)
     else: 
@@ -103,14 +133,18 @@ def find_ranges(segments):
     return segment_array
 
 def make_searchable_ranges(residue_range):
-    #the output from find_segments
+    """The function takes the output range from find_ranges() and
+    convert this to an array of AA positions that can be searched."""
     
+    #create an empty list
     a_list = []
     
+    #create a for loop to convert each residue range into an array
     for jj in range(len(residue_range)):
         
         ranges_ = np.arange(residue_range[jj][0], residue_range[jj][1]+1)
         
+        #append each range
         a_list.append(ranges_)
     
     ranges_ = np.hstack(a_list)
@@ -118,9 +152,20 @@ def make_searchable_ranges(residue_range):
     return ranges_
 
 def find_mutational_sites(mutation_location, range_of_model):
+    """A function that takes each mutational location and the range of 
+    the PDB model and controls if the PDB model covers the mutational site. 
+    The output is twofold:
+        
+    mut_result = True or False, if a specific mutation is present in pdb 
+    mutation_location = The mutational location associated with each true or false
     
+    """
+    
+    #empty list of collect the presence of mutations iteratively
     presence_of_mutations = []
-    
+
+    #for loop over the mutations to decide on a binary scale if the 
+    # mutational location exist in the PDB file. 
     for mutation in range(len(mutation_location)):
         
         if mutation_location[mutation] in range_of_model:
@@ -137,7 +182,43 @@ def find_mutational_sites(mutation_location, range_of_model):
     else: 
         mut_result = True
     
-    return mut_result
+    return mut_result, mutation_location
+
+def find_mutational_sites_2nd(mutation_location, range_of_model, iteration):
+    """A function that is not yet utilized - the aim is to have a function
+    that can find mutational sites, if a specific model does cover two out 
+    of three mutations, perhaps this solved PDB is better than a homolog.
+    """
+    
+    #create an empty list to store the presence of mutations, presence = 1, missing = 0
+    presence_of_mutations = []
+    
+    sites = []
+    
+    #a for loop to investigate if the mutational sites (locations) are present in the model
+    for mutation in range(len(mutation_location)):
+        
+        #when the mutational sites are present or not present, this is stored in a binary list as 1 or 0. 
+        if mutation_location[mutation] in range_of_model:
+            r = 1
+            #when a site has been found, the information is stored in case not all mutations can be found.
+            sites.append(mutation_location[mutation])
+        
+        else:
+            r = 0
+            
+        presence_of_mutations.append(r)
+
+    #for loop to ensure, that even if not all mutations are found, the best model 
+    #that satisfy most of the criteria is chosen  
+    if sum(presence_of_mutations) >= len(presence_of_mutations)-iteration:
+        mut_result = True
+
+    else:
+        mut_result = False
+
+    return mut_result, sites
+
 
 ##################################################################################
 #Slimfast Functions
@@ -216,8 +297,32 @@ def get_uniprot_segments(pdb_id, uniprot_id):
 
 def validate_sequence(path, input_df):
     
+    """
+    A wrapper function to validate the sequence of each of the PDBs
+    found in "find_structure.py"
+    
+    The input is:
+        
+        path = the path where the input file is.
+        input_df = the input file
+        
+    The output is:
+        
+        pdb_uniprot_df = a csv file in the following format:
+            
+        Uniprot | PDB | found_mutational_sites         
+        
+    """
+    
+    
     #create an empty list to hold the found pdb id's
     pdb_list = []
+    
+    #create an empty list to hold the found pdb id's
+    mutational_sites = []
+    
+    #create an empty list for the Uniprot IDs where the pdbs does not cover the mutational sites
+    # uniprot_missing_mut_site = []
     
     path = path + "/structure_lists"
     
@@ -254,15 +359,39 @@ def validate_sequence(path, input_df):
             
             #if all the mutational sites realated to a specific uniprot ID is found in a singular PDB model, 
             #the model is kept, if not the search contunues. 
-            if placement == True:
+            if placement[0] == True:
                 pdb_list.append(r_list[j]) 
+                mutational_sites.append(placement[1])
                 break
+            
             else:
                 continue
-    
-    pdb_uniprot_df = pd.DataFrame(list(zip(names[1], pdb_list)), columns =['Uniprot_ID', 'PDB'])   
-    pdb_uniprot_df.to_csv("pdb_uniprot_dataframe.csv")      
-    
-    #something if they are not found - start over, look for all -1 ? 
+                
+#   For later;          
+#            if placement[0] == False:
+#                placement_ = find_mutational_sites_2nd(mutations[names[1][i]], searchable_range, j)
+#                
+#                if placement_[0] == True:
+#                    pdb_list.append(r_list[j]) 
+#                    mutational_sites.append(placement_[1])
+#                    break
+                    
+#                else:
+#                    continue
             
-    return(pdb_uniprot_df)
+#            if placement[0] == False:
+#                uniprot_missing_mut_site.append(r_list[j])
+                 
+    #write into a file
+#    textfile = open("missing_mutational_sites.txt", "w")
+#    for element in uniprot_missing_mut_site:
+#        textfile.write(element + "\n")
+#        textfile.close()
+    
+    pdb_uniprot_df = pd.DataFrame(list(zip(names[1], pdb_list, mutational_sites)), 
+                                  columns =['Uniprot', 'PDB', 'found_mutational_sites'])         
+    
+    pdb_uniprot_df.to_csv("pdb_uniprot_df.csv")
+            
+    return(pdb_uniprot_df)  
+
