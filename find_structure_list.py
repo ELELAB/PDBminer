8c437f7014f30256d16c73cac3266fe206d50ee8
DegnKristine <84312237+DegnKristine@users.noreply.github.com>
2021-10-07 11:23:57 +0200

    massive updates for the second review.

--- find_structure_list.py
+++ find_structure_list.py
@@ -12,7 +12,7 @@ from SLiM_codes import get_pdbs, get_structure_metadata
 
 #############
 
-def get_structure_df(uniprot_id):
+def get_structure_df(uniprot_id, cluster_id):
     """ This function takes a single uniprot ID and outputs a 
     dataframe containing a sorted list of PDB ids and their metadata 
     associated to a singular Uniprot ID. 
@@ -31,6 +31,9 @@ def get_structure_df(uniprot_id):
     #find all pdbs for a uniprot id
     pdb_list = get_pdbs(uniprot_id)
     
+    #empty list for collection of PDBs passing the first quality checkpoint. 
+    currated_pdb_list = []
+    
     if len(pdb_list) == 0:
         return uniprot_id
     
@@ -40,19 +43,26 @@ def get_structure_df(uniprot_id):
     
         #make a for loop to populate the empty df with metadata for each pdb
         for pdb in pdb_list:
-        
+            
             #retrive all metadata        
             structure_metadata = get_structure_metadata(pdb)
+    
+            #Removing empty files such as PDBid 1OVC that where captured.
+            if type(structure_metadata) == dict:
+                #input the values into a dataframe
+                structure_metadata_df = pd.DataFrame.from_dict(dict([(x, [k[x] for k in [structure_metadata]]) for x in structure_metadata]))
+
+                #append values
+                structure_df = structure_df.append(structure_metadata_df)
+                currated_pdb_list.append(pdb)
         
-            #input the values into a dataframe
-            structure_metadata_df = pd.DataFrame.from_dict(dict([(x, [k[x] for k in [structure_metadata]]) for x in structure_metadata]))
-        
-            #append values
-            structure_df = structure_df.append(structure_metadata_df) 
-        
+            else: 
+                continue
+                
         #Add pdb ID & uniprot ID as columns
-        structure_df.index = pdb_list
+        structure_df.index = currated_pdb_list
         structure_df.insert(0, "Uniprot_ID", [uniprot_id]*len(structure_df), True)
+        structure_df.insert(1, "ClusterID", [cluster_id]*len(structure_df), True)
     
         #Annotated structures as mutated or not-mutated
         structure_df["model_mutated"] = structure_df["model_mutated"].apply(lambda x: 'Not-Mutated' if x is None else 'Mutated')
@@ -84,8 +94,8 @@ def get_structure_df(uniprot_id):
         structure_df = structure_df.sort_values(by="Rank")
         structure_df = structure_df.drop(['Rank'], axis=1)
                                                             
-        return structure_df                                                    
-
+        return structure_df          
+                                          
                                             
 def find_structure_list(input_dataframe):
     """Takes the input file and the path where it is placed and outputs
@@ -112,8 +122,11 @@ def find_structure_list(input_dataframe):
     
     with open("missing_ID.txt", "w") as textfile:
         
-        for uniprot_id in range(len(all_uniprot_ids)):
-            structure_info = get_structure_df(all_uniprot_ids[uniprot_id])
+        for row in range(len(all_uniprot_ids)):
+            
+            print(all_uniprot_ids[row])
+            
+            structure_info = get_structure_df(all_uniprot_ids[row], input_dataframe.ClusterID[row])
         
             if type(structure_info) != str: 
                 df_collector.append(structure_info)
@@ -124,13 +137,3 @@ def find_structure_list(input_dataframe):
     found_structure_list = pd.concat(df_collector)    
             
     return found_structure_list
- 
-
-
-
-
-
-
-
-
-
