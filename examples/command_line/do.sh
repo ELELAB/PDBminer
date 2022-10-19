9f24030a649c7fa408fc6e8305e4146226dcb02e
Kristine Degn <krde@kb-bioinfo01.cancer.dk>
2022-10-19 08:21:32 +0200

    snakemake file placement alter

--- examples/command_line/do.sh
+++ examples/command_line/do.sh
@@ -1,5 +1,5 @@
 module load conda/4.9.2/modulefile
 conda activate PDBminer
 
-../../PDBminer -g SCP2 -u P22307 -n 1
+../../PDBminer -g SCP2 -u P22307 -n 1 -f ../../program/snakefile
 
--- examples/input_file/do.sh
+++ examples/input_file/do.sh
@@ -1,5 +1,7 @@
 module load conda/4.9.2/modulefile
 conda activate PDBminer
 
-../../PDBminer -i input_file.csv -n 1
+#require input file to run
+
+../../PDBminer -i input_file.csv -n 1  -f ../../program/snakefile
 
