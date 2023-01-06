3b7f2c00948e9a29a54af2cecdedffbd80454426
Kristine Degn <krde@kb-bioinfo01.cancer.dk>
2023-01-06 09:54:26 +0100

    rerun of examples

--- examples/command_line/do.sh
+++ examples/command_line/do.sh
@@ -3,3 +3,5 @@ conda activate PDBminer
 
 ../../PDBminer -g SCP2 -u P22307 -n 1 -f ../../program/snakefile
 ../../PDBminer2coverage
+../../PDBminer2network
+
--- examples/different_clusters/do.sh
+++ examples/different_clusters/do.sh
@@ -3,3 +3,5 @@ conda activate PDBminer
 
 ../../PDBminer -i input_file.csv -n 1 -f ../../program/snakefile
 ../../PDBminer2coverage
+../../PDBminer2network
+
