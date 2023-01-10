027d4b14acc9b9c5054b146e47fee0a229ae6e95
Kristine Degn <krde@kb-bioinfo01.cancer.dk>
2023-01-10 10:30:43 +0100

    remove line activating env

--- examples/command_line/do.sh
+++ examples/command_line/do.sh
@@ -1,5 +1,3 @@
-conda activate PDBminer
-
 ../../PDBminer -g SCP2 -u P22307 -n 1 -f ../../program/snakefile
 ../../PDBminer2coverage
 ../../PDBminer2network
--- examples/input_file/do.sh
+++ examples/input_file/do.sh
@@ -1,7 +1,3 @@
-conda activate PDBminer
-
-#require input file to run
-
 ../../PDBminer -i input_file.csv -n 1  -f ../../program/snakefile
 ../../PDBminer2coverage
 ../../PDBminer2network
